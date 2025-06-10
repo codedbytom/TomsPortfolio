1e228d12b752bc3ff8dc0800cb0320b12029a9f3
Tom Evanko <tevanko@outlook.com>
2025-06-10 14:54:37 -0400

    Trying to get preview working

--- tomsportfolio/vite.config.js
+++ tomsportfolio/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import plugin from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
@@ -8,10 +9,17 @@ export default defineConfig(({ command, mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
 
     return {
-        plugins: [plugin()],
+        plugins: [plugin(), createHtmlPlugin({
+      inject: {
+        data: {
+          domain: env.VITE_PUBLIC_DOMAIN,
+        },
+      },
+    })],
         server: {
             port: 11534,
-            strictPort: true
+            strictPort: true,
+            middlewareMode: false
         },
         define: {
             // Make env variables available to the app
