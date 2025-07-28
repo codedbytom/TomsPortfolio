dbb38c46734669261b90bd80431338ee0014c30b
Tom Evanko <tevanko@outlook.com>
2025-07-28 14:09:03 -0400

    Updated the naming of Coding Nightmares

--- tomsportfolio/src/App.jsx
+++ tomsportfolio/src/App.jsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from './context/ThemeContext';
 import Home from './pages/Home';
 import Resume from './pages/Resume';
 import Hobbies from './pages/Hobbies';
-import CodingNightmares from './pages/CodingNightmares';
+import CodingChallenges from './pages/CodingChallenges';
 import OptIn from './pages/TextDemo/OptIn';
 import SmsPreview from './pages/TextDemo/SmsPreview';
 import TermsAndConditions from './pages/Legal/TermsAndConditions';
@@ -25,7 +25,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/resume" element={<Resume />} />
         <Route path="/hobbies" element={<Hobbies />} />
-        <Route path="/coding-nightmares" element={<CodingNightmares />} />
+        <Route path="/coding-challenges" element={<CodingChallenges />} />
         <Route path="/text-demo/opt-in" element={<OptIn />} />
         <Route path="/text-demo/sms-preview" element={<SmsPreview />} />
         <Route path="/text-demo/survey/:responseGuid" element={<Survey />} />
